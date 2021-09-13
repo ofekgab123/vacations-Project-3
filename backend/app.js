98e8feaa54ffd8e9a5af44f85f355ef758c792fc
ofekgab123 <ofekgabay14@gmail.com>
2021-09-13 22:01:01 +0300

    save

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-const PORT =3000;
+const PORT = process.env.Port || 3000;
 globalThis.config = require(process.env.NODE_ENV === "production"
 ? "./configs/config-prod.json"
 : "./configs/config-dev.json");
